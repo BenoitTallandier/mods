074440acff6104fb51859b11fb8b5b38b5a3940a
benoitTallandier <benoit.tallandier@gmail.com>
2017-03-21 20:32:42 +0100

    debut mods

--- js/script.js
+++ js/script.js
@@ -2,4 +2,22 @@ $(window).load(function(){
 });
 $(window).ready(function(){
     $('.modal-trigger').leanModal();
+    $('.materialboxed').materialbox();
+
+    $('.modBox').click(function(){
+        $("#modsList").removeClass("fadeInLeft");
+        $("#modsList").addClass("fadeOutLeft");
+        $('#modsDetail').removeClass("fadeOutRight");
+        $('#modsDetail').addClass("fadeInRight");
+    });
+    $(window).keyup(function(event){
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == '27'){
+            $("#modsList").removeClass("fadeOutLeft");
+            $("#modsList").addClass("fadeInLeft");
+            $('#modsDetail').removeClass("fadeInRight");
+            $('#modsDetail').addClass("fadeOutRight");
+        }
+    });
+
 });
