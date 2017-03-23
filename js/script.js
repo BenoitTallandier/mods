a6119cd6af6f85a09494094ef06aa092a7983c20
benoitTallandier <benoit.tallandier@gmail.com>
2017-03-23 13:52:19 +0100

    correction modsDetails

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 $(window).load(function(){
+    $('#modsDetail').hide();
 });
 $(window).ready(function(){
     $('.modal-trigger').leanModal();
     $('.materialboxed').materialbox();
-
-    $('.modBox').click(function(){
-        $("#modsList").removeClass("fadeInLeft");
-        $("#modsList").addClass("fadeOutLeft");
-        $('#modsDetail').removeClass("fadeOutRight");
-        $('#modsDetail').addClass("fadeInRight");
-    });
+    $('.modBox').click(seeDetailMod);
     $(window).keyup(function(event){
 		var keycode = (event.keyCode ? event.keyCode : event.which);
 		if(keycode == '27'){
-            $("#modsList").removeClass("fadeOutLeft");
-            $("#modsList").addClass("fadeInLeft");
-            $('#modsDetail').removeClass("fadeInRight");
-            $('#modsDetail').addClass("fadeOutRight");
+            retourMod();
         }
     });
+    $('#backMod').click(retourMod);
 
 });
+
+function seeDetailMod(){
+    $("#modsList").removeClass("fadeInLeft");
+    $("#modsList").addClass("fadeOutLeft");
+    $('#modsDetail').show();
+    $('#modsDetail').removeClass("fadeOutRight");
+    $('#modsDetail').addClass("fadeInRight");
+}
+function retourMod(){
+    $("#modsList").removeClass("fadeOutLeft");
+    $("#modsList").addClass("fadeInLeft");
+    $('#modsDetail').removeClass("fadeInRight");
+    $('#modsDetail').addClass("fadeOutRight");
+    $('#modsDetail').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){ if($('#modsDetail').hasClass("fadeOutRight")){$('#modsDetail').hide();}});
+}
